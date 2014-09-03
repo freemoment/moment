b17b0ee7adc13e15dd7ba1144fd202e29c1b4d40
Harvey <harvey@Harveys-MacBook-Pro.local>
2014-09-03 22:48:49 +0800

    第一个可用的真实函数
    切换有伞没伞 switchStatus

--- umbrella-cloud-code/cloud/main.js
+++ umbrella-cloud-code/cloud/main.js
@@ -1,6 +1,47 @@
+Parse.Cloud.define("switchStatus", function(request, response) {
+                   
+                   var userId = request.params.userId;
+                   var userQuery = new Parse.Query(Parse.User);
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("test", function(request, response) {
-  response.success("My Umbrella!");
+                   userQuery.get(userId, {
+                                 success: function(user) {
+                                 var query = new Parse.Query('UmbrellaContribution');
+                                 query.equalTo('user', user);
+                                 query.first({
+                                             success: function(contribution) {
+                                                var isContributor = contribution.get('isContributor');
+                                                if(isContributor) {
+                                                    contribution.set('isContributor', false);
+                                                } else {
+                                                    contribution.set('isContributor', true);
+                                                }
+                                             contribution.save().then(function(){
+                                                                        response.success("switched");
+                                                                      },function(error){
+                                                                        response.error(error);
+                                                                      });
+                                             },
+                                             error: function(error) {
+                                                response.error(error);
+                                             }
+                                 });
+                                 },
+                                 error: function(user, error) {
+                                    response.error(error);
+                                 }
+                   });
+});
+
+Parse.Cloud.define("findAllInquiries", function(request, response) {
+                   var Relationship = Parse.Object.extend('Relationship');
+                   var query = new Parse.Query(Relationship);
+                   query.equalTo('fromUser', Parse.User.current());
+                   query.find({
+                              success: function(relationships) {
+                                response.success(relationships);
+                              },
+                              error: function(error) {
+                                response.error(error);
+                              }
+                   });
 });
